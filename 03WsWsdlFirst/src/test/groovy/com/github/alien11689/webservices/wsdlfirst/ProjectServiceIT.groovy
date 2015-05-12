f9089d127cebe1be4e4e50ebd7007b4ef134dd14
Dominik Adam Przybysz <alien11689@gmail.com>
2015-05-12 23:45:05 +0200

    Refactor ws tests

--- 03WsWsdlFirst/src/test/groovy/com/github/alien11689/webservices/wsdlfirst/ProjectServiceIT.groovy
+++ 03WsWsdlFirst/src/test/groovy/com/github/alien11689/webservices/wsdlfirst/ProjectServiceIT.groovy
@@ -15,29 +15,39 @@ class ProjectServiceIT extends Specification {
         client.deleteAll(new DeleteAllRequest())
     }
 
-    def "should talk with Project Service"() {
-        when:
-            GetProjectsResponse getProjectsResponse = client.getProjects(new GetProjectsRequest())
-        then:
-            getProjectsResponse.project == []
+    def "should get empty project list at the beggining"() {
+        expect:
+            client.getProjects(new GetProjectsRequest()).project == []
+    }
+
+    def "should create project with success"() {
         when:
             client.createProject(new CreateProjectRequest(project: new Project(name: "Test project")), new User(login: "admin", email: 'admin@test.org'))
         then:
             Project project = client.getProject(new GetProjectRequest(name: "Test project")).project
             project.getName() == "Test project"
             project.getOwner()?.getLogin() == "admin"
+    }
+
+    def "should throw fault when trying to create the same project second time"() {
         when:
+            client.createProject(new CreateProjectRequest(project: new Project(name: "Test project")), new User(login: "admin", email: 'admin@test.org'))
             client.createProject(new CreateProjectRequest(project: new Project(name: "Test project")), new User(login: "admin", email: 'admin@test.org'))
         then:
             Fault_Exception createException = thrown(Fault_Exception)
             createException.faultInfo.code == BigInteger.valueOf(2)
+    }
+
+    def "should throw fault when getting not existing project"(){
         when:
             client.getProject(new GetProjectRequest(name: "Unknown")).project
         then:
             Fault_Exception getException = thrown(Fault_Exception)
             getException.faultInfo.code == BigInteger.ONE
-        when:"dry run"
-            client.deleteAll(new DeleteAllRequest())
+    }
+
+    def "should create project in tryRun mode so project will not be created"() {
+        when: "dry run"
             ((BindingProvider) client).getRequestContext().put(Message.PROTOCOL_HEADERS as String, ["dryRun": ["true"]])
             client.createProject(new CreateProjectRequest(project: new Project(name: "Test project")), new User(login: "admin", email: 'admin@test.org'))
         then:
